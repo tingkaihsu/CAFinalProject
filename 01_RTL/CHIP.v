d9ea64fa19b68ddbc991453a20adc304c27a468b
tingkaihsu <tingkaihsu0924@gmail.com>
2024-06-13 19:58:20 +0800

    use my structure

--- 01_RTL/CHIP.v
+++ 01_RTL/CHIP.v
@@ -181,38 +181,7 @@ module CHIP #(                                                                  
         imem_cen = 1;
         instr = i_IMEM_data;
         next_PC = (i_DMEM_stall)? PC : PC+4;
-        if(i_DMEM_stall) begin
-            finish = 0;
-            wrt_to_rd = 0;
-            mem_cen_nxt = 0;
-            mem_wen_nxt = 0;
-            opcode = instr[6:0];
-            func3 = instr[14:12];
-            func7 = instr[BIT_W-1:25];
-            rs1 = instr[19:15];
-            rs2 = instr[24:20];
-            rd = instr[11:7];
-            mul_valid = 0;
-            mul_mode = 3;
-            mul_in_a = 0;
-            mul_in_b = 0;
-            wrt_to_rd = 0;
-            mem_wdata = rs2_data;
-            if(opcode == LW) begin
-                immd = instr[BIT_W-1:20];
-                dmem_addr = $signed(rs1_data) + $signed(immd);
-            end
-            else if(opcode == SW) begin
-                immd = {instr[BIT_W-1:25], instr[11:7]};
-                dmem_addr = $signed(rs1_data) + $signed(immd);
-            end
-            else begin
-                dmem_addr = 0;
-            end
-            write_data = 0;
-            immd = 0;
-        end
-        else if(i_rst_n)begin
+        if (!i_DMEM_stall) begin
             finish = 0;
             opcode = instr[6:0];
             func3 = instr[14:12];
@@ -355,7 +324,6 @@ module CHIP #(                                                                  
                         mem_cen_nxt = 1;
                         next_PC = PC;
                     end
-                    
                 end
                 SW: begin
                     immd = {instr[BIT_W-1:25], instr[11:7]};
@@ -415,29 +383,297 @@ module CHIP #(                                                                  
             endcase
         end
         else begin
-            next_PC = 0;
+            next_PC = PC;
             finish = 0;
-            opcode = 0;
-            func3 = 0;
-            func7 = 0;
-            rs1 = 0;
-            rs2 = 0;
-            rd = 0;
             wrt_to_rd = 0;
             mul_valid = 0;
             mul_mode = 3;
             mul_in_a = 0;
             mul_in_b = 0;
-            dmem_addr = 0;
-            mem_wdata = 0;
+            
+            opcode = instr[6:0];
+            func3 = instr[14:12];
+            func7 = instr[BIT_W-1:25];
+            rs1 = instr[19:15];
+            rs2 = instr[24:20];
+            rd = instr[11:7];
+            write_data = 0;
+
+            mem_wdata = rs2_data;
             mem_rdata = 0;
             mem_cen_nxt = 0;
             mem_wen_nxt = 0;
-            write_data = 0;
+            if (opcode == LW) begin
+                immd = instr[BIT_W-1:20];
+                dmem_addr = $signed(rs1_data) + $signed(immd);
+            end
+            else if (opcode == SW) begin
+                immd = {instr[BIT_W-1:25], instr[11:7]};
+                dmem_addr = $signed(rs1_data) + $signed(immd);
+            end
+            else begin
+                immd = 0;
+                dmem_addr = 0;
+            end
             immd = 0;
         end
-    end
 
+        // if(i_DMEM_stall) begin
+        //     finish = 0;
+        //     wrt_to_rd = 0;
+        //     mem_cen_nxt = 0;
+        //     mem_wen_nxt = 0;
+        //     opcode = instr[6:0];
+        //     func3 = instr[14:12];
+        //     func7 = instr[BIT_W-1:25];
+        //     rs1 = instr[19:15];
+        //     rs2 = instr[24:20];
+        //     rd = instr[11:7];
+        //     mul_valid = 0;
+        //     mul_mode = 3;
+        //     mul_in_a = 0;
+        //     mul_in_b = 0;
+        //     wrt_to_rd = 0;
+        //     mem_wdata = rs2_data;
+        //     if(opcode == LW) begin
+        //         immd = instr[BIT_W-1:20];
+        //         dmem_addr = $signed(rs1_data) + $signed(immd);
+        //     end
+        //     else if(opcode == SW) begin
+        //         immd = {instr[BIT_W-1:25], instr[11:7]};
+        //         dmem_addr = $signed(rs1_data) + $signed(immd);
+        //     end
+        //     else begin
+        //         dmem_addr = 0;
+        //     end
+        //     write_data = 0;
+        //     immd = 0;
+        // end
+        // else if(i_rst_n)begin
+        //     finish = 0;
+        //     opcode = instr[6:0];
+        //     func3 = instr[14:12];
+        //     func7 = instr[BIT_W-1:25];
+        //     rs1 = instr[19:15];
+        //     rs2 = instr[24:20];
+        //     rd = instr[11:7];
+        //     wrt_to_rd = 0;
+        //     mul_valid = 0;
+        //     mul_mode = 3;
+        //     mul_in_a = 0;
+        //     mul_in_b = 0;
+        //     dmem_addr = 0;
+        //     mem_wdata = 0;
+        //     mem_rdata = 0;
+        //     mem_cen_nxt = 0;
+        //     mem_wen_nxt = 0;
+        //     write_data = 0;
+        //     immd = 0;
+        //     case(opcode) 
+        //         AUIPC: begin //auipc
+        //             wrt_to_rd = 1;
+        //             immd[19:0] = instr[BIT_W-1:12];
+        //             write_data = PC + {immd[BIT_W-1:12], 12'b0};
+        //         end
+        //         JAL: begin //jal
+        //             wrt_to_rd = 1;
+        //             next_PC = $signed(PC) + $signed({instr[BIT_W-1], instr[19:12], instr[20], instr[30:21], 1'b0});
+        //             write_data = PC + 4;
+        //         end
+        //         JALR: begin //jalr
+        //             wrt_to_rd = 1;
+        //             immd[11:0] = instr[BIT_W-1:20];
+        //             next_PC = $signed(rs1_data) + $signed(immd[11:0]);
+        //             write_data = PC + 4;
+        //         end
+        //         ASAXM: begin // add, sub, and, xor
+        //             case({func3, func7})
+        //                 {ADD_FUNCT3, ADD_FUNCT7}: begin
+        //                     wrt_to_rd = 1;
+        //                     write_data = $signed(rs1_data) + $signed(rs2_data);
+        //                     // dealing with overflow
+        //                 end
+        //                 {SUB_FUNCT3, SUB_FUNCT7}: begin
+        //                     wrt_to_rd = 1;
+        //                     write_data = $signed(rs1_data) - $signed(rs2_data);
+        //                 end
+        //                 {AND_FUNCT3, ADD_FUNCT7}: begin
+        //                     wrt_to_rd = 1;
+        //                     write_data = rs1_data & rs2_data;
+        //                 end
+        //                 {XOR_FUNCT3, XOR_FUNCT7}: begin
+        //                     wrt_to_rd = 1;
+        //                     write_data = rs1_data ^ rs2_data;
+        //                 end
+        //                 /*
+        //                 {DIV_FUNCT3, DIV_FUNCT7}: begin
+        //                     wrt_to_rd = 0;
+        //                     mul_in_a = rs1_data;
+        //                     mul_in_b = rs2_data;
+        //                     //$display("mul_valid = %d", mul_valid);
+        //                     if (mul_ready) begin
+        //                         next_PC = PC + 4;
+        //                         wrt_to_rd = 1;
+        //                         mul_mode = 3;
+        //                         mul_valid = 0;
+        //                     end
+        //                     else begin
+        //                         next_PC = PC;
+        //                         mul_mode = 1;
+        //                         mul_valid = 1;
+        //                         wrt_to_rd = 0;
+        //                     end
+        //                     write_data = mul_result[BIT_W-1:0];
+        //                 end
+        //                 */
+        //                 {MUL_FUNCT3, MUL_FUNCT7}: begin
+        //                     wrt_to_rd = 0;
+        //                     mul_in_a = rs1_data;
+        //                     mul_in_b = rs2_data;
+        //                     //$display("mul_valid = %d", mul_valid);
+        //                     if (mul_ready) begin
+        //                         next_PC = PC + 4;
+        //                         wrt_to_rd = 1;
+        //                         mul_mode = 3;
+        //                         mul_valid = 0;
+        //                     end
+        //                     else begin
+        //                         next_PC = PC;
+        //                         mul_mode = 2;
+        //                         mul_valid = 1;
+        //                         wrt_to_rd = 0;
+        //                     end
+        //                     write_data = mul_result[BIT_W-1:0];
+        //                 end
+        //                 default: begin
+        //                     next_PC = PC + 4;
+        //                     wrt_to_rd = 0;
+        //                     write_data = 0;
+        //                 end
+        //             endcase
+        //         end
+        //         IMMD_OP: begin
+        //             case (func3)
+        //                 ADDI_FUNCT3: begin //addi
+        //                     wrt_to_rd = 1;
+        //                     write_data = $signed(rs1_data) + $signed(instr[BIT_W-1:20]);
+        //                 end
+        //                 SLLI_FUNCT3: begin //slli
+        //                     wrt_to_rd = 1;
+        //                     write_data = rs1_data << $unsigned(instr[24:20]);
+        //                 end
+        //                 SLTI_FUNCT3: begin //slti
+        //                     wrt_to_rd = 1;
+        //                     write_data = ($signed(rs1_data) < $signed(instr[BIT_W-1:20]))? 1 : 0;
+        //                 end
+        //                 SRAI_FUNCT3: begin //srai
+        //                     wrt_to_rd = 1;
+        //                     write_data = rs1_data >> $unsigned(instr[24:20]);
+        //                 end
+        //                 default: begin
+        //                     next_PC = PC + 4;
+        //                     wrt_to_rd = 0;
+        //                     write_data = 0;
+        //                     immd = 0;
+        //                 end
+        //             endcase
+        //         end
+        //         LW: begin 
+        //             immd = instr[BIT_W-1:20];
+        //             dmem_addr = $signed(rs1_data) + $signed(immd);
+        //             write_data = i_DMEM_rdata;
+        //             wrt_to_rd = 1;
+        //             if(!i_DMEM_stall && stall_counter > 0) begin
+        //                 mem_wen_nxt = 0;
+        //                 mem_cen_nxt = 0;
+        //             end
+        //             else begin
+        //                 mem_wen_nxt = 0;
+        //                 mem_cen_nxt = 1;
+        //                 next_PC = PC;
+        //             end
+        //         end
+        //         SW: begin
+        //             immd = {instr[BIT_W-1:25], instr[11:7]};
+        //             dmem_addr = $signed(rs1_data) + $signed(immd);
+        //             mem_wdata = rs2_data;
+        //             wrt_to_rd = 0;
+        //             if(!i_DMEM_stall && stall_counter > 0) begin
+        //                 mem_wen_nxt = 0;
+        //                 mem_cen_nxt = 0;
+        //             end
+        //             else begin
+        //                 mem_cen_nxt = 1;
+        //                 mem_wen_nxt = 1;
+        //                 next_PC = PC;
+        //             end
+        //         end
+        //         BRNCH: begin //beq, bge, blt, bne
+        //             immd = {instr[BIT_W-1], instr[7], instr[30:25], instr[11:8], 1'b0};
+        //             case(func3)
+        //                 BEQ_FUNCT3: begin //beq
+        //                     if(rs1_data == rs2_data) next_PC = $signed(PC) + $signed(immd);
+        //                     else next_PC = PC + 4;
+        //                 end
+        //                 BGE_FUNCT3: begin //bge
+        //                     if($signed(rs1_data) >= $signed(rs2_data)) next_PC = $signed(PC) + $signed(immd);
+        //                     else next_PC = PC + 4;
+        //                 end
+        //                 BLT_FUNCT3: begin //blt
+        //                     if($signed(rs1_data) < $signed(rs2_data)) next_PC = $signed(PC) + $signed(immd);
+        //                     else next_PC = PC + 4;
+        //                 end
+        //                 BNE_FUNCT3: begin //bne
+        //                     if(rs1_data != rs2_data) next_PC = $signed(PC) + $signed(immd);
+        //                     else next_PC = PC + 4;
+        //                 end
+        //                 default: begin
+        //                     next_PC = PC + 4;
+        //                 end
+        //             endcase
+        //         end
+        //         ECALL: begin //ecall
+        //             wrt_to_rd = 0;
+        //             finish = 1;
+        //         end
+        //         default: begin
+        //             next_PC = PC + 4;
+        //             wrt_to_rd = 0;
+        //             write_data = 0;
+        //             immd = 0;
+        //             finish = 0;
+        //             dmem_addr = 0;
+        //             mem_wdata = 0;
+        //             mem_rdata = 0;
+        //             mem_cen_nxt = 0;
+        //             mem_wen_nxt = 0;
+        //         end
+        //     endcase
+        // end
+        // else begin
+        //     next_PC = 0;
+        //     finish = 0;
+        //     opcode = 0;
+        //     func3 = 0;
+        //     func7 = 0;
+        //     rs1 = 0;
+        //     rs2 = 0;
+        //     rd = 0;
+        //     wrt_to_rd = 0;
+        //     mul_valid = 0;
+        //     mul_mode = 3;
+        //     mul_in_a = 0;
+        //     mul_in_b = 0;
+        //     dmem_addr = 0;
+        //     mem_wdata = 0;
+        //     mem_rdata = 0;
+        //     mem_cen_nxt = 0;
+        //     mem_wen_nxt = 0;
+        //     write_data = 0;
+        //     immd = 0;
+        // end
+        // $display("well done!");
+    end
 endmodule
 
 module Reg_file(i_clk, i_rst_n, wen, rs1, rs2, rd, wdata, rdata1, rdata2);
